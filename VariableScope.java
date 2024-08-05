9c657bd50988274b54b86908b84d393fc5f9cfb9
harleen <harleen.johal31@gmail.com>
2024-08-05 12:02:40 +0530

    VariableScope

---
+++ VariableScope.java
@@ -0,0 +1,23 @@
+public class VariableScope {
+    static int staticVar = 100;
+
+    int instanceVar = 200;
+
+    void myMethod() {
+        int localVar = 300;
+        if (true) {
+            int blockVar = 400;
+            System.out.println("Inside block: blockVar = " + blockVar);
+        }
+        System.out.println("Inside method: localVar = " + localVar);
+    }
+
+    public static void main(String[] args) {
+        VariableScope obj = new VariableScope();
+        obj.myMethod();
+
+        System.out.println("Static variable: staticVar = " + VariableScope.staticVar);
+
+        System.out.println("Instance variable: instanceVar = " + obj.instanceVar);
+    }
+}
